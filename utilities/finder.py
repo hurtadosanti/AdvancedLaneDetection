93b746945a2270b6efa40cd5c10cf7c8b9e60c79
Santiago Hurtado <s-hurtad@uniandes.edu.co>
2020-09-10 15:02:12 +0200

    feat(finder): Draw lines with frame average

--- utilities/finder.py
+++ utilities/finder.py
@@ -6,14 +6,18 @@ from utilities import image_utility
 
 class LaneFinder:
     def __init__(self):
-        self.inspection_image = None
         self.result_image = None
+        self.inspection_image = None
         self.left_fit = None
         self.right_fit = None
+        self.average_left = []
+        self.average_right = []
+        self.average_y = []
+        self.frames_stacked = 0
 
     def _find_lanes_windows(self, image, number_windows: int = 9, margin: int = 100, min_pixels: int = 50):
         """Return left_x,left_y,right_x,right_y"""
-        self.inspection_image = np.dstack((image, image, image)) * 255
+
         left_lane_idx = []
         right_lane_idx = []
         # Get left and right lanes sections to process
@@ -30,6 +34,9 @@ class LaneFinder:
         non_zero_y = np.array(non_zero[0])
         non_zero_x = np.array(non_zero[1])
 
+        inspection_image = np.dstack((image, image, image)) * 255
+        self.result_image = np.zeros_like(inspection_image)
+
         # process each window to find where the line is
         for window in range(number_windows):
             # Identify window boundaries in x and y (and right and left)
@@ -40,14 +47,9 @@ class LaneFinder:
             win_x_right_low = right_current - margin
             win_x_right_high = right_current + margin
             # Draw the windows on the visualization image
-            cv2.rectangle(self.inspection_image, (win_x_left_low, win_y_low),
-                          (win_x_left_high, win_y_high), (0, 255, 0), 2)
-            cv2.rectangle(self.inspection_image, (win_x_right_low, win_y_low),
-                          (win_x_right_high, win_y_high), (0, 255, 0), 2)
-            good_left_idx = ((non_zero_y >= win_y_low) & (non_zero_y < win_y_high) & (non_zero_x >= win_x_left_low) &
-                             (non_zero_x < win_x_left_high)).nonzero()[0]
-            good_right_idx = ((non_zero_y >= win_y_low) & (non_zero_y < win_y_high) & (non_zero_x >= win_x_right_low) &
-                              (non_zero_x < win_x_right_high)).nonzero()[0]
+            good_left_idx, good_right_idx = self.draw_windows(inspection_image, non_zero_x, non_zero_y, win_x_left_high,
+                                                              win_x_left_low, win_x_right_high, win_x_right_low,
+                                                              win_y_high, win_y_low)
             # Append these indices to the lists
             left_lane_idx.append(good_left_idx)
             right_lane_idx.append(good_right_idx)
@@ -69,19 +71,32 @@ class LaneFinder:
         left_y = non_zero_y[left_lane_idx]
         right_x = non_zero_x[right_lane_idx]
         right_y = non_zero_y[right_lane_idx]
+        self.inspection_image = inspection_image
         return left_x, left_y, right_x, right_y
+
+    def draw_windows(self, inspection_image, non_zero_x, non_zero_y, win_x_left_high, win_x_left_low, win_x_right_high,
+                     win_x_right_low, win_y_high, win_y_low):
+        cv2.rectangle(inspection_image, (win_x_left_low, win_y_low),
+                      (win_x_left_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(inspection_image, (win_x_right_low, win_y_low),
+                      (win_x_right_high, win_y_high), (0, 255, 0), 2)
+        good_left_idx = ((non_zero_y >= win_y_low) & (non_zero_y < win_y_high) & (non_zero_x >= win_x_left_low) &
+                         (non_zero_x < win_x_left_high)).nonzero()[0]
+        good_right_idx = ((non_zero_y >= win_y_low) & (non_zero_y < win_y_high) & (non_zero_x >= win_x_right_low) &
+                          (non_zero_x < win_x_right_high)).nonzero()[0]
+        return good_left_idx, good_right_idx
 
     def plot_polylines(self, image):
         # Find Lane sections
         left_x, left_y, right_x, right_y = self._find_lanes_windows(image)
         # Color each side
-        self.inspection_image[left_y, left_x] = [255, 0, 0]
-        self.inspection_image[right_y, right_x] = [0, 0, 255]
+        self.result_image[left_y, left_x] = [255, 0, 0]
+        self.result_image[right_y, right_x] = [0, 0, 255]
         # Generate x and y values for plotting
         plot_y = np.linspace(0, image.shape[0] - 1, image.shape[0])
         left_fit_x, right_fit_x = self._fit_poly(plot_y, left_x, left_y, right_x, right_y)
-        self.draw_lane_section(left_fit_x, plot_y, self.inspection_image, right_fit_x)
-        return plot_y, left_fit_x, right_fit_x
+        self.append_lanes(plot_y, left_fit_x, right_fit_x)
+        return self.draw_lane_average()
 
     def search_around_polylines(self, image, margin=100):
         # Grab activated pixels
@@ -115,23 +130,37 @@ class LaneFinder:
         # Calculate the windows to search the points
         left_line_window1 = np.array([np.transpose(np.vstack([left_fit_x - margin, plot_y]))])
         left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fit_x + margin, plot_y])))])
-        left_line_pts = np.hstack((left_line_window1, left_line_window2))
+
         right_line_window1 = np.array([np.transpose(np.vstack([right_fit_x - margin, plot_y]))])
         right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fit_x + margin, plot_y])))])
         # Add to the original image
+
+        left_line_pts = np.hstack((left_line_window1, left_line_window2))
         right_line_pts = np.hstack((right_line_window1, right_line_window2))
         cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
         cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
-        self.inspection_image = cv2.addWeighted(result_img, 1, window_img, 0.3, 0)
-        self.draw_lane_section(left_fit_x, plot_y, result_img, right_fit_x)
-        return result_img
+        self.result_image = cv2.addWeighted(result_img, 1, window_img, 0.3, 0)
 
-    def draw_lane_section(self, left_fit_x, plot_y, result_img, right_fit_x):
+        self.append_lanes(plot_y, left_fit_x, right_fit_x)
+
+        return self.draw_lane_average()
+
+    def draw_lane_section(self, plot_y, left_fit_x, right_fit_x):
         # Draw the lane section
         pts_left = np.array([np.transpose(np.vstack([left_fit_x, plot_y]))])
         pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fit_x, plot_y])))])
         pts = np.hstack((pts_left, pts_right))
-        cv2.fillPoly(result_img, np.int_([pts]), (0, 255, 0))
+        cv2.fillPoly(self.result_image, np.int_([pts]), (0, 255, 0))
+
+    def draw_lane_average(self):
+        left_fit_x = np.average(self.average_left, axis=0)
+        right_fit_x = np.average(self.average_right, axis=0)
+        plot_y = np.average(self.average_y, axis=0)
+        pts_left = np.array([np.transpose(np.vstack([left_fit_x, plot_y]))])
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fit_x, plot_y])))])
+        pts = np.hstack((pts_left, pts_right))
+        cv2.fillPoly(self.result_image, np.int_([pts]), (0, 255, 0))
+        return left_fit_x,right_fit_x,plot_y
 
     def _fit_poly(self, plot_y, left_x, left_y, right_x, right_y):
         self.left_fit = np.polyfit(left_y, left_x, 2)
@@ -140,3 +169,14 @@ class LaneFinder:
         left_fit_x = self.left_fit[0] * plot_y ** 2 + self.left_fit[1] * plot_y + self.left_fit[2]
         right_fit_x = self.right_fit[0] * plot_y ** 2 + self.right_fit[1] * plot_y + self.right_fit[2]
         return left_fit_x, right_fit_x
+
+    def append_lanes(self, plot_y, left_fit_x, right_fit_x):
+        self.average_left.append(left_fit_x)
+        self.average_right.append(right_fit_x)
+        self.average_y.append(plot_y)
+        self.frames_stacked = self.frames_stacked + 1
+        if self.frames_stacked >= 5:
+            self.average_y.pop(0)
+            self.average_left.pop(0)
+            self.average_right.pop(0)
+            self.frames_stacked = self.frames_stacked - 1
--- video_pipeline.py
+++ video_pipeline.py
@@ -20,36 +20,33 @@ class VideoLaneLineDetection:
         self.calibration = c
         self.cap = cv2.VideoCapture(video_path)
 
+
     def process_video(self):
+        lane_finder = finder.LaneFinder()
         frame_width = int(self.cap.get(3))
         frame_height = int(self.cap.get(4))
         out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+        first_frame = True
         while True:
             ret, frame = self.cap.read()
             if not ret:
                 cv2.destroyAllWindows()
                 return
-
             undistorted = self.calibration.undistort_image(frame)
             warped, reverse = image_utility.warp_image(undistorted, 140, 470, 100)
             combined_binary = thresholds.highlight_features(warped, 7, (50, 150), (50, 200), (0.5, np.pi / 2),
                                                             (80, 200))
-            lanes = finder.LaneFinder(combined_binary)
-            plot_y, left_fit, right_fit = lanes.fit_polynomial()
+            if first_frame:
+                plot_y, left_fit_x, right_fit_x = lane_finder.plot_polylines(combined_binary)
+                first_frame = False
+            else:
+                plot_y, left_fit_x, right_fit_x = lane_finder.search_around_polylines(combined_binary, 25)
 
-            left, right = image_utility.measure_curvature_real(plot_y, left_fit, right_fit)
-            pos = image_utility.measure_vehicle_distance(frame.shape[0], frame.shape[1], left_fit, right_fit)
-            text = f'left curvature:{left:.2f} right curvature:{right:.2f} center offset:{pos:.2f}'
-            write_text(frame, text, 50, 50)
-
-            window_lanes, result_img, left_line_pts, right_line_pts = lanes.search_around_poly(
-                combined_binary)
-
-            reversed_frame = image_utility.reverse_warp(frame, result_img, reverse, (frame.shape[1], frame.shape[0]),
+            reversed_frame = image_utility.reverse_warp(frame, lane_finder.result_image, reverse,
+                                                        (frame.shape[1], frame.shape[0]),
                                                         0.3)
 
             cv2.imshow('lanes', reversed_frame)
-
             out.write(reversed_frame)
             cv2.waitKey(1)
 
